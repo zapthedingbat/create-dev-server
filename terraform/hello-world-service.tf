0b6426491fbc3377fcbe2c9955ef7ce83bafa7f3
Sam Greenhalgh <sam@zapthedingbat.com>
2019-12-16 17:00:19 +0000

    remove hello world

--- terraform/hello-world-service.tf
+++ terraform/hello-world-service.tf
@@ -1,8 +1,8 @@
-resource "docker_service" "hello_world" {
-  name = "hello-world"
-  task_spec {
-    container_spec {
-      image = "hello-world"
-    }
-  }
-}
+# resource "docker_service" "hello_world" {
+#   name = "hello-world"
+#   task_spec {
+#     container_spec {
+#       image = "hello-world"
+#     }
+#   }
+# }
