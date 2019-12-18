2d7509d2359b6f24c94a1f9111ea991ac17981db
Sam Greenhalgh <sam@zapthedingbat.com>
2019-12-18 17:01:11 +0000

    fix labels

--- terraform/services/traefik/main.tf
+++ terraform/services/traefik/main.tf
@@ -8,14 +8,13 @@ module "service" {
   dns-name   = "traefik"
   domain     = var.domain
   image      = "traefik:v2.1.1"
-  port       = 1234
+  port       = 80
   labels = {
+    "traefik.http.routers.traefik.service"                             = "api@internal"
     "traefik.http.middlewares.redirect-to-https.redirectscheme.scheme" = "https"
-    "traefik.http.routers.api.service"                                 = "api@internal"
     "traefik.http.routers.http-catchall.entrypoints"                   = "http"
     "traefik.http.routers.http-catchall.middlewares"                   = "redirect-to-https@docker"
     "traefik.http.routers.http-catchall.rule"                          = "hostregexp(`{host:.+}`)"
-    "traefik.http.services.placeholder.loadbalancer.server.port"       = "1234"
   }
   ports = [80, 443]
   args = [
