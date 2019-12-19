75c61786653aae88c6171df094509f8512ec80d2
Sam Greenhalgh <sam@zapthedingbat.com>
2019-12-19 08:52:28 +0000

    add portainer

--- terraform/services/main.tf
+++ terraform/services/main.tf
@@ -11,3 +11,11 @@ module "traefik" {
   network_name = docker_network.public_network.name
   network_id   = docker_network.public_network.id
 }
+
+module "portainer" {
+  source = "./portainer"
+
+  domain       = var.domain
+  network_name = docker_network.public_network.name
+  network_id   = docker_network.public_network.id
+}
---
+++ terraform/services/portainer/main.tf
@@ -0,0 +1,25 @@
+resource "docker_volume" "portainer" {
+  name = "portainer"
+}
+
+module "service" {
+  source     = "../../modules/docker_service"
+  network_id = var.network_id
+  dns-name   = "portainer"
+  domain     = var.domain
+  image      = "portainer/portainer:1.23.0"
+  port       = 9000
+  ports      = [9000]
+  mounts = [
+    {
+      target = "/var/run/docker.sock"
+      source = "/var/run/docker.sock"
+      type   = "bind"
+    },
+    {
+      target = "/data"
+      source = "${docker_volume.portainer.name}"
+      type   = "volume"
+    }
+  ]
+}
