139efb77274e9da3c939824ebccf28986c41d4b1
Sam Greenhalgh <sam@zapthedingbat.com>
2019-12-19 12:16:38 +0000

    pass password hash as environment var (not secret)

--- terraform/services/portainer/main.tf
+++ terraform/services/portainer/main.tf
@@ -2,29 +2,6 @@ resource "docker_volume" "portainer" {
   name = "portainer"
 }
 
-data "template_file" "portainer_templates_tpl" {
-  template = "${file("templates/templates.json.tpl")}"
-
-  vars {
-    domain = var.domain
-  }
-}
-
-resource "docker_secret" "portainer_admin_secret" {
-  name = "admin-secret"
-  data = "${base64encode(var.admin_password_hash)}"
-}
-
-resource "docker_config" "portainer_templates" {
-  name = "portainer-templates-${replace(timestamp(), ":", ".")}"
-  data = "${base64encode(data.template_file.portainer_templates_tpl.rendered)}"
-
-  lifecycle {
-    ignore_changes        = ["name"]
-    create_before_destroy = true
-  }
-}
-
 module "service" {
   source     = "../../modules/docker_service"
   network_id = var.network_id
@@ -49,11 +26,5 @@ module "service" {
       source = "${docker_volume.portainer.name}"
       type   = "volume"
     }
-  ]
-  configs = [
-    {
-      config_id = "${docker_config.portainer_templates.id}"
-      file_name = "/templates.json"
-    },
   ]
 }
--- terraform/services/variables.tf
+++ terraform/services/variables.tf
@@ -1,3 +1,7 @@
 variable "domain" {
   type = string
 }
+
+variable "portainer_admin_password_hash" {
+  type = string
+}
