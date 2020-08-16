c5bcd2642d7d056cd3af953d39cb5decd1ae3423
Sam Greenhalgh <sam@zapthedingbat.com>
2020-08-16 09:39:50 +0100

    fix cleanup network_id vars

--- terraform/services/traefik/variables.tf
+++ terraform/services/traefik/variables.tf
@@ -2,9 +2,9 @@ variable "domain" {
   type = string
 }
 
-variable "network_id" {
-  type = string
-}
+# variable "network_id" {
+#   type = string
+# }
 
 variable "network_name" {
   type = string
